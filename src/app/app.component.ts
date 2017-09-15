ca4515a735c6ffa9b107ed578685766d8498e375
VitalieBraga <vitaliebraga@gmail.com>
2017-09-15 11:56:38 -0700

    Finished assignmnet US Time Zones Display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'US Time Zone Display';
+  selected: string = "";
+  time: string;
+  
+  getTime(zone: string) {
+    if (zone === "PST") {
+      this.selected = "PST";
+      this.time = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+    } else if (zone === "MST") {
+      this.selected = "MST"
+      this.time = new Date().toLocaleString("en-US", {timeZone: "America/Boise"});
+    } else if (zone === "CST") {
+      this.selected = "CST"
+      this.time = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
+    } else if (zone === "EST") {
+      this.selected = "EST"
+      this.time = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+    }
+  };
+
+  clear() {
+    this.selected = "";
+    this.time = "";
+  };
+
 }
